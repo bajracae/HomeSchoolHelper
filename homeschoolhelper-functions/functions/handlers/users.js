6c358135400fd54d5842eb871a9ad7ba0fb045d2
Aeijan <bajracae@oregonstate.edu>
2020-09-17 16:45:18 -0700

    End of part 13

--- homeschoolhelper-functions/functions/handlers/users.js
+++ homeschoolhelper-functions/functions/handlers/users.js
@@ -60,7 +60,7 @@ exports.signup =  (req, res) => {
           return res.status(400).json({email: 'Email is already in use'});
         }
         else{
-          return res.status(500).json({error: err.code});
+          return res.status(500).json({general: 'Something went wrong, please try again'});
         }
       });
   };
@@ -85,12 +85,11 @@ exports.signup =  (req, res) => {
       })
       .catch(err => {
         console.error(err);
-        if(err.code === 'auth/wrong-password'){
-          return res.status(403).json({general: 'Wrong credientials, please try again'});
-        }
-        else{
-          return res.status(500).json({error: err.code});
-        }
+        // auth/wrong-password
+        // auth/user-not-user
+          return res
+          .status(403)
+          .json({general: 'Wrong credientials, please try again'});
       });
   };
 
@@ -107,6 +106,43 @@ exports.signup =  (req, res) => {
         return res.status(500).json({error: err.code});
     });
   };
+
+// Get any user's details
+exports.getUserDetails = (req, res) => {
+  let userData = {};
+  db.doc(`/users/${req.params.handle}`)
+  .get()
+  .then(doc => {
+    if(doc.exists){
+      userData.user = doc.data();
+      return db.collection('screams').where('userHandle', '==', req.params.handle)
+      .orderBy('createdAt', 'desc')
+      .get();
+    }
+    else {
+      return res.status(404).json({error: 'User not found'});
+    }
+  })
+  .then(data => {
+    userData.screams = [];
+    data.forEach(doc => {
+      userData.screams.push({
+        body: doc.data().body,
+        createdAt: doc.data().createdAt,
+        userHandle: doc.data().userHandle,
+        userImage: doc.data().userImage,
+        likeCount: doc.data().likeCount,
+        commentCount: doc.data().commentCount,
+        screamId: doc.id
+      })
+    });
+    return res.json(userData);
+  })
+  .catch(err => {
+    console.error(err);
+    return res.status(500).json({error:err.code});
+  })
+}
 
 // Get own user details
 exports.getAuthenticatedUser = (req, res) => {
@@ -207,4 +243,20 @@ exports.getAuthenticatedUser = (req, res) => {
         });
     });
     busboy.end(req.rawBody);
+  };
+
+  exports.markNotificationsRead = (req, res) => {
+    let batch = db.batch();
+    req.body.forEach(notificationId => {
+      const notification = db.doc(`/notifications/${notificationId}`);
+      batch.update(notification, {read: true});
+    });
+    batch.commit()
+    .then(() => {
+      return res.json({message: 'Notifications marked read'});
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({error: err.code});
+    });
   };
--- homeschoolhelper-functions/functions/index.js
+++ homeschoolhelper-functions/functions/index.js
@@ -10,13 +10,15 @@ const { getAllScreams,
         commentOnScream,
         likeScream,
         unlikeScream,
-        deleteScream
+        deleteScream,
       } = require('./handlers/screams');
 const { signup,
         login,
         uploadImage,
         addUserDetails,
-        getAuthenticatedUser
+        getAuthenticatedUser,
+        getUserDetails,
+        markNotificationsRead
       } = require('./handlers/users');
 
 // Scream routes
@@ -34,6 +36,8 @@ app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
+app.get('/user/:handle', getUserDetails);
+app.post('/notifications', FBAuth, markNotificationsRead);
 
 exports.api = functions.https.onRequest(app);
 
@@ -95,4 +99,56 @@ exports.createNotificationOnComment = functions
       console.error(err);
       return;
     });
+});
+
+exports.onUserImageChange = functions.region('us-east1').firestore.document('/users/{userId}')
+.onUpdate((change) => {
+  console.log(change.before.data());
+  console.log(change.after.data());
+  if(change.before.data().imageUrl !== change.after.data().imageUrl){
+    console.log('image has changed');
+    const batch = db.batch();
+    return db
+    .collection('screams')
+    .where('userHandle', '==', change.before.data().handle)
+    .get()
+    .then((data) => {
+      data.forEach(doc => {
+        const scream = db.doc(`/screams/${doc.id}`);
+        batch.update(scream, {userImage: change.after.data().imageUrl});
+      });
+      return batch.commit();
+    });
+  }
+  else {
+    return true;
+  }
+});
+
+exports.onScreamDelete = functions
+.region('us-east1').firestore
+.document('/screams/{screamId}')
+.onDelete((snapshot, context) => {
+  const screamId = context.params.screamId;
+  const batch = db.batch();
+  return db.collection('comments').where('screamId', '==', screamId).get()
+  .then(data => {
+    data.forEach(doc => {
+      batch.delete(db.doc(`/comments/${doc.id}`));
+    })
+    return db.collection('likes').where('screamId', '==', screamId).get();
+  })
+  .then(data => {
+    data.forEach(doc => {
+      batch.delete(db.doc(`/likes/${doc.id}`));
+    })
+    return db.collection('notifications').where('screamId', '==', screamId).get();
+  })
+  .then(data => {
+    data.forEach(doc => {
+      batch.delete(db.doc(`/notifications/${doc.id}`));
+    })
+    return batch.commit();
+  })
+  .catch(err => console.error(err));
 });
