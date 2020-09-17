024f713207be075bc27a9260efec89d818a1d949
Aeijan <bajracae@oregonstate.edu>
2020-09-17 13:58:20 -0700

    error: function ignored because the firestore emulator does not exist or is not running

---
+++ homeschoolhelper-functions/functions/util/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apiKey: "AIzaSyDDZ9QQq3ERy5fQRISfQL-hGhmRcZGLdJU",
+    authDomain: "homeschoolhelper-f87ed.firebaseapp.com",
+    databaseURL: "https://homeschoolhelper-f87ed.firebaseio.com",
+    projectId: "homeschoolhelper-f87ed",
+    storageBucket: "homeschoolhelper-f87ed.appspot.com",
+    messagingSenderId: "272235939372",
+    appId: "1:272235939372:web:ceaf8b436856a7b470f389",
+    measurementId: "G-6VBTGF8CEM"
+  };
+  
